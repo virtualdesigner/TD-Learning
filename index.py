8b2c6dcd4451aacb71bbbe0ee5aeceb2b63007e2
virtualdesigner <smvirtualdesigner@gmail.com>
2020-12-30 12:50:15 +0530

    Name the axes.

--- index.py
+++ index.py
@@ -17,7 +17,6 @@ reward_list = []
 
 def epsilon_greedy_policy(no_of_actions):
     def generate_action(epsilon, action_value_of_s):
-        print(epsilon)
         uniform_prob = epsilon/(no_of_actions + 1)
         return np.random.choice([0, 1, 2, 3, 4, 5, np.argmax(action_value_of_s)], p=[uniform_prob, uniform_prob, uniform_prob,
                                                                                      uniform_prob, uniform_prob, uniform_prob, (uniform_prob)+(1-epsilon)])
@@ -50,9 +49,12 @@ for episode in range(1, total_episodes):
         total_reward += reward
 
     epsilon = (1/(episode))
+    print(learning_rate, epsilon)
 
     reward_list.append(total_reward)
 env.close()
 
+plt.xlabel('Episode', fontsize=18)
+plt.ylabel('Reward', fontsize=18)
 plt.plot([j for j in range(1, total_episodes)], reward_list)
 plt.show()
