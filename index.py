7886d8d3f1e6ff0a37641baba77ac977bb898812
virtualdesigner <smvirtualdesigner@gmail.com>
2020-12-31 14:30:47 +0530

    Implement SARSA & Q-Learning algorithms on Taxi-v3 which is a gym environment built by OPENAI.

--- index.py
+++ index.py
@@ -3,15 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 env = gym.make('Taxi-v3')
-
-q_table = np.zeros((500, 6))
-
-# hyperparameters
-gamma = 0.8
-learning_rate = 0.3
-epsilon = 1
-total_episodes = 500
-trajectory_length = 500
 reward_list = []
 
 
@@ -23,38 +14,46 @@ def epsilon_greedy_policy(no_of_actions):
     return generate_action
 
 
-generate_action = epsilon_greedy_policy(6)
+def TD(on_policy=False, epsilon=1, total_episodes=500, trajectory_length=500, gamma=0.9, learning_rate=0.6, q_table=np.zeros((500, 6))):
 
+    generate_action = epsilon_greedy_policy(6)
 
-for episode in range(1, total_episodes):
-    observation = env.reset()
-    done = False
-    total_reward = 0
-    current_action = generate_action(epsilon, q_table[observation])
-
-    for transition in range(trajectory_length):
-        env.render()
-
-        old_observation = observation
-
-        action_took = current_action
-
-        observation, reward, done, info = env.step(current_action)
-
+    for episode in range(1, total_episodes):
+        observation = env.reset()
+        total_reward = 0
         current_action = generate_action(epsilon, q_table[observation])
 
-        q_table[old_observation, action_took] += learning_rate * (reward + (
-            gamma * q_table[observation, current_action]) - q_table[old_observation, action_took])
+        for transition in range(trajectory_length):
+            # env.render()
 
-        total_reward += reward
+            old_observation = observation
 
-    epsilon = (1/(episode))
-    print(learning_rate, epsilon)
+            action_took = current_action
 
-    reward_list.append(total_reward)
+            observation, reward, done, info = env.step(current_action)
+
+            if(on_policy == True):
+                # SARSA
+                current_action = generate_action(epsilon, q_table[observation])
+            else:
+                # Q-Learning
+                current_action = np.argmax(q_table[observation])
+
+            q_table[old_observation, action_took] += learning_rate * (reward + (
+                gamma * q_table[observation, current_action]) - q_table[old_observation, action_took])
+
+            total_reward += reward
+
+        epsilon = (1/(episode))
+
+        reward_list.append(total_reward)
+
+
+TD()
+
 env.close()
 
 plt.xlabel('Episode', fontsize=18)
 plt.ylabel('Reward', fontsize=18)
-plt.plot([j for j in range(1, total_episodes)], reward_list)
+plt.plot([j for j in range(1, 500)], reward_list)
 plt.show()
--- readme.md
+++ readme.md
@@ -1,15 +1,22 @@
-<h2>Implemented Q-learning algorithm on Taxi-V3 which is an OPENAI gym environment.</h2>
+<h1>Implemented Q-learning algorithm on Taxi-V3 which is an OPENAI gym environment.</h1>
 <br>
-<h3>Hyperparameters:</h3>
+<h2>Hyperparameters:</h2>
 <ul>
     <li>Episodes - 500</li>
     <li>Trajectory length - 500</li>
-    <li>Learning rate - 0.3</li>
-    <li>Gamma - 0.8</li>
+    <li>Learning rate - 0.6</li>
+    <li>Gamma - 0.9</li>
     <li>Initial epsilon - 1</li>
     <li>Epsilon decay function - 1/episode_number</li>
 </ul>
 <br>
 
+<h2>SARSA</h2>
 
-![Graph showing the reward after an episode](./taxi_v3_q_learning.png)
+![SARSA Graph](./taxi_v3_sarsa.png)
+
+<br>
+
+<h2>Q-Learning</h2>
+
+![Q-Learning Graph](./taxi_v3_q_learning.png)
